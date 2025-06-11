5919e19f517f5ec4321cf1f992ab75d914fb822d
rakesh <nxtwave@ip-10-2-168-76.ap-south-1.compute.internal>
2025-06-11 05:18:18 +0000

    Added CSS

--- src/components/RandomNumberGenerator/index.js
+++ src/components/RandomNumberGenerator/index.js
@@ -3,20 +3,25 @@ import {Component} from 'react'
 
 import './index.css'
 
-
-class RandomNumberGenerator extends Component{
-    render(){
-        return (
-            <div>
-                <div>
-                    <h1> Random Number </h1>
-                    <p> Generate a random number in the range of 0 to 100 </p>
-                    <button type="button"> Generate </button>
-                    <p> 0 </p>
-                </div>
-            </div>
-        )
-    }
+class RandomNumberGenerator extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="random-number-generator-container">
+          <h1 className="heading"> Random Number </h1>
+          <p className="description">
+            {' '}
+            Generate a random number in the range of 0 to 100{' '}
+          </p>
+          <button type="button" className="generate-buttom">
+            {' '}
+            Generate{' '}
+          </button>
+          <p className="random-number"> 0 </p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default RandomNumberGenerator
